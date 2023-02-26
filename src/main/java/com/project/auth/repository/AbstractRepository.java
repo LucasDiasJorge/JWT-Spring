49a39371d55ae052d0d068b3483fcb37dec2aaa8
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-02-26 17:48:54 -0300

    Refactoring

--- src/main/java/com/project/auth/repository/AbstractRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.auth.repository;
-
-import com.project.auth.model.AbstractModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AbstractRepository extends JpaRepository<AbstractModel, Long> {
-
-}
