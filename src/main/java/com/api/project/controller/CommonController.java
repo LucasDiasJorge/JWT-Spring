fa244c81e63db1d0e168acbbb50a45e86d773c99
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-25 19:04:42 -0300

    Using http utils in ping route

--- src/main/java/com/api/project/controller/CommonController.java
+++ src/main/java/com/api/project/controller/CommonController.java
@@ -1,25 +1,35 @@
 package com.api.project.controller;
 
-import com.api.project.model.UserModel;
-import org.springframework.http.ResponseEntity;
+import com.api.project.HttpUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping(("/api/common"))
 public class CommonController {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public CommonController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/ping")
-    public Map<String,Object> ping(){
+    public Map<String,Object> ping(HttpServletRequest request){
 
         Map<String,Object> ret = new HashMap<>();
+
+        String ip = HttpUtils.getRequestIP(request);
+
+        System.out.println("From:" + ip);
 
         ret.put("Version", "1.0.0.0.0");
 
